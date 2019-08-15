a48c9e00e9080caad9df981310442c5b2b9fea21
Roel Pasetes <rp@drproveit.com>
2019-08-15 01:14:22 -0500

    Added models, views, configuration, and dummy map using Leaflet.js With upload form.

--- incidentsmap/cad/models.py
+++ incidentsmap/cad/models.py
@@ -1,10 +1,50 @@
 from django.db import models
 
 class Parcel(models.Model):
-    pass
+    '''
+    parcel enrichment
+    '''
+    parcel_owner_name = models.CharField(max_length=100)
+    parcel_mail_address = models.TextField()
+    parcel_land_value = models.FloatField()
+    parcel_land_sq_ft = models.FloatField()
+    
 
 class Incident(models.Model):
-    # contains parcel
-    # can extract attributes for map.
-    pass
+    '''
+    I'm taking the most interesting parts of the incidents to show
+    and mapping them to a field in the db.
+    '''
+    incident_number = models.CharField(max_length=12)
+
+    # taken from address
+    incident_latitude = models.FloatField()
+    incident_longitude = models.FloatField()
+
+    # concatted value for display.
+    incident_address_string = models.CharField(max_length=200)
+
+    # TODO: change to enums
+    incident_type = models.CharField(max_length=100)
+    incident_sub_type = models.CharField(max_length=100)
+    incident_day_of_week = models.CharField(max_length=8)
+
+    incident_event_opened = models.DateTimeField(auto_now_add=False) 
+    incident_event_closed = models.DateTimeField(auto_now_add=False) 
+    incident_response_zone = models.CharField(max_length=10)
+    incident_parcel = models.ForeignKey(Parcel, on_delete=models.CASCADE)
+    
+    # TODO: change to array
+    incident_units_involved = models.CharField(max_length=200)
+
+    '''
+    there are others we can add here, like all the different 
+    units that responded.
+    '''
+
+    # Enrichment
+    # -- Weather
+    incident_weather_description = models.TextField()
+
+
 
--- incidentsmap/cad/views.py
+++ incidentsmap/cad/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from .enricher import Enricher
 
 def index(request):
-    return HttpResponse("Dummy for now.")
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        enricher = Enricher(filename)
+        return render(request, 'cad/index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    else:
+        template = loader.get_template('cad/index.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
 
